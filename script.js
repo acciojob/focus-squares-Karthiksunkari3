e19e13b1908e8f10aaba4e4753a3100e9661f06a
Karthik Sunkari <84893245+Karthiksunkari3@users.noreply.github.com>
2023-05-20 01:09:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-//your JS code here. If required.
+function changeOtherSquaresColor(square) {
+  var squares = document.getElementsByClassName('square');
+  for (var i = 0; i < squares.length; i++) {
+    if (squares[i] !== square) {
+      squares[i].classList.add('highlight');
+    }
+  }
+}
+
+function resetSquaresColor() {
+  var squares = document.getElementsByClassName('square');
+  for (var i = 0; i < squares.length; i++) {
+    squares[i].classList.remove('highlight');
+  }
+}
